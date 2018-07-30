6cd4176f9409e660d9b16883a4674bb97b42e987
Muhammad Yusry Hadi <myh.myh09@yahoo.com>
2018-07-30 17:31:55 +0800

    Include string trimming

--- MainProg.cpp
+++ MainProg.cpp
@@ -10,18 +10,74 @@ using namespace std;
 
 bool MainFallbackResp;
 
-string concatID;
+string concatID,level,username;
 void Dashboard ()
 {
-	
+	if (level == "1") //admin
+	{
+		cout << "\n Welcome, " << username;
+//		cout << 
+	}
+	else if (level == "2") // Manager
+	{
+			
+	}
+	else if (level == "3") // Staff
+	{
+		int resp;
+		int counter =0;
+		while (counter==0)	
+		{
+		cout << "\n Welcome, " << username;
+		cout << "\n 1. View Transaction";
+		cout << "\n 2. Record Transaction";
+		cout << "\n 3. Warranty Claim";
+		cout << "\n 4. Print Report";
+		cin >>  resp;
+			switch (resp) {
+				case 1 : {
+					
+					counter++;
+					break;
+				}
+				case 2 : {
+					
+					counter++;
+					break;
+				}
+				case 3 : {
+					
+					counter++;
+					break;
+				}
+				case 4 : {
+					
+					counter++;
+					break;
+				}
+				default :
+					{
+						cout << "\n Invalid Input";
+					}
+			};
+		}
+	}
+	else 
+		cout << "\n UNAUTHORIZED ACCESS";
 }
 void Keyword(ifstream & stream, string token, string fx) { //Search for keyword/username inside text file database
-    string line; 
+    string line,levln; 
     while (getline(stream, line)) {
         if (line.find(token) != string::npos) {
             cout << line << endl;
-            
+            levln=line;
             if (fx == "sublogin") {
+            	int length = line.length(); // Get User Row length
+            	
+            	levln.erase(levln.begin(), levln.end()-1);
+            	level = levln;
+            	
+            	line.erase(line.begin()+(length-1), line.end()); //remove ID Column inside user row (set cursor to 2nd last char and erase last char)
         		std::string::iterator end_pos = std::remove(line.begin(), line.end(), ' '); //remove trailling/any white spaces in between (eg admin Admin123 --> adminAdmin123)
 				line.erase(end_pos, line.end()); //remove the spaces
             	cout << line;
@@ -40,7 +96,7 @@ void login()
 {
 	char trackingNo[10],Resp;
 	string fxName;
-	string username,password,creds_db,creds;
+	string password,creds_db,creds;
 	cout << "\n Choose Login type \n 1. Staff \n 2. Customers? Track yur order Here. \n Response >> ";
 	cin >> Resp;
 	
@@ -117,6 +173,9 @@ int main()
 	}
 	system("CLS");
 	cout << "Login Success";
+	
+	Dashboard();
+	
 	
 	return 0;
 }
